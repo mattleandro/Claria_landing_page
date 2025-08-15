e4df77eba3db068bcc2062b376634c5aec8d1c37
Matheus Leandro <matheus022leandro@gmail.com>
2025-08-15 17:30:43 -0300

    ajustes nos links

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -113,7 +113,7 @@ function Contact({ id }) {
           <span className={styles['icon-label']}>{t('contactEmailLabelText')}</span>
         </a>
         {/* Youtube Icon */}
-        <a href="http://youtube.com/your-youtube-channel" target="_blank" rel="noopener noreferrer" className={styles['icon-item']} aria-label={t('contactYoutubeAria')}>
+        <a href="https://www.youtube.com/channel/UCGssOBGUZmHcqGH7yEj-ZKg" target="_blank" rel="noopener noreferrer" className={styles['icon-item']} aria-label={t('contactYoutubeAria')}>
           <div className={styles['icon-wrapper']}>
             <svg viewBox="0 0 24 24" className={styles['social-icon']}>
               <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
@@ -122,7 +122,7 @@ function Contact({ id }) {
           <span className={styles['icon-label']}>{t('contactYoutubeLabel')}</span>
         </a>
         {/* Spotify Icon */}
-        <a href="https://open.spotify.com/intl-pt/album/7sNlDAVnqTf44AsqzYvGc0" target="_blank" rel="noopener noreferrer" className={styles['icon-item']} aria-label={t('contactSpotifyAria')}>
+        <a href="https://open.spotify.com/intl-pt/artist/6Za8rqeiBcShSX1tzkUqkl" target="_blank" rel="noopener noreferrer" className={styles['icon-item']} aria-label={t('contactSpotifyAria')}>
           <div className={styles['icon-wrapper']}>
             <svg viewBox="0 0 24 24" className={styles['social-icon']}>
               <path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.779-.179-.899-.539-.12-.421.18-.78.54-.9 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.301 1.02zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.28-1.02 15.721 1.621.539.3.719 1.02.419 1.56-.299.421-1.02.599-1.559.3z" />
@@ -131,7 +131,7 @@ function Contact({ id }) {
           <span className={styles['icon-label']}>{t('contactSpotifyLabel')}</span>
         </a>
         {/* Hyperddit Icon */}
-        <a href="https://hypeddit.com/jc5rhf" target="_blank" rel="noopener noreferrer" className={styles['icon-item']} aria-label={t('contactHyperdditAria')}>
+        <a href="https://hypeddit.com/" target="_blank" rel="noopener noreferrer" className={styles['icon-item']} aria-label={t('contactHyperdditAria')}>
           <div className={styles['icon-wrapper']}>
             <svg viewBox="0 0 24 24" className={styles['social-icon']}>
               <path d="M0 0v24h24V0H0zm4.602 4.025h2.244c.509 0 .716.215.716.717v5.64h8.883v-5.64c0-.509.215-.717.717-.717h2.229c.5 0 .71.23.724.717v14.516c0 .509-.215.717-.717.717h-2.23c-.51 0-.717-.215-.717-.717v-5.735H7.562v5.735c0 .516-.215.717-.716.717H4.602c-.51 0-.717-.208-.717-.717V4.742c0-.509.207-.717.717-.717z" />
